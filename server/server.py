61822077e7eb44444b8aa9948eaf960acac80088
Your Name <georges.abitbole.08@gmail.com>
2024-10-01 11:31:25 +0200

    changes for static folder and some files

--- server/server.py
+++ server/server.py
@@ -28,7 +28,7 @@ stripe.api_version = '2024-06-20'
 stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
 static_dir = str(os.path.abspath(os.path.join(__file__ , "..", os.getenv("STATIC_DIR"))))
 # app = Flask(__name__, static_folder=static_dir, static_url_path="", template_folder=static_dir)
-app = Flask(__name__, static_folder=static_dir, static_url_path="", template_folder='../client')
+app = Flask(__name__, static_folder='../client', static_url_path="", template_folder='../client')
 
 
 # app.config['SECRET_KEY'] = "tsfyguaistyatuis589566875623568956"
